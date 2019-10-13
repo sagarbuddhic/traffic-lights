4a0cc7739d15020963e5c8da6cc70d50e37853d1
sagarbuddhic <sagar.buddhic@gmail.com>
2019-10-13 18:23:01 +1100

    Updates

---
+++ precache-manifest.be9009a78cd89d2aa0dc2ccbdbb09ac8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a682f60c811afbfebf805d3a4bf88f53",
+    "url": "/traffic-lights/index.html"
+  },
+  {
+    "revision": "e7ced4a50c16519f3266",
+    "url": "/traffic-lights/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4ee0d79a4ae79cf5a156",
+    "url": "/traffic-lights/static/js/2.766065f5.chunk.js"
+  },
+  {
+    "revision": "e7ced4a50c16519f3266",
+    "url": "/traffic-lights/static/js/main.08cc2d03.chunk.js"
+  },
+  {
+    "revision": "c00ce7272f2760cd6a22",
+    "url": "/traffic-lights/static/js/runtime-main.9c00f843.js"
+  }
+]);
